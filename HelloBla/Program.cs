551df594ca1438dc532561d9164a453c4fc3be04
Benedikt Schlager <benedikt.schlager@outlook.at>
2019-11-07 20:51:52 +0100

    Get Level 2 passing <3

--- HelloBla/Program.cs
+++ HelloBla/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using static System.Math;
 
 namespace HelloBla
 {
     class Program
     {
+        public static double FULLCIRCLE => 2 * Math.PI;
+
         static void Main(string[] args)
         {
+            var rawInput = @"1.00 1.00 30.00
+2.13 4.30 23.00
+1.75 3.14 -23.00
+2.70 45.00 -34.00
+4.20 -5.30 20.00
+9.53 8.12 0.00"; // -> 0.00 8.12 0.00
+
+            var input = ParseCC(rawInput.Split('\n'), 3, a => (WheelBase: DoubleParse(a[0]), Distance: DoubleParse(a[1]), SteeringAngle: DoubleParse(a[2])));
+
+            /*
             Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
             Console.WriteLine(Turnradius(1, 13.76d / 360 * 2 * PI));
             Console.WriteLine(Turnradius(1, 2.34d / 360 * 2 * PI));
@@ -15,8 +29,54 @@ namespace HelloBla
             Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
             Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
             Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
+            */
             Console.WriteLine("Level 2");
-            Console.WriteLine(Level2Neu(1d, 1d, Grd2Rad(30)));
+            foreach (var i in input)
+            {
+                WriteLineCC(i);
+                WriteLineCC(Level2Neu(i.WheelBase, i.Distance, Grd2Rad(i.SteeringAngle)), true);
+            }
+           
+
+            //Console.WriteLine(Level2Neu(9.53d, 8.12d, 0.00d));
+        }
+
+        private static string AsCCDouble(double x) => string.Format(CultureInfo.InvariantCulture, "{0:0.00}", x);
+
+        private static void WriteLineCC((double WheelBase, double Distance, double SteeringAngle) i, bool output=false)
+        {
+            var c = Console.ForegroundColor;
+            if (output)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.WriteLine($"{AsCCDouble(i.WheelBase)} {AsCCDouble(i.Distance)} {AsCCDouble(i.SteeringAngle)}");
+            if (output)
+            {
+                Console.ForegroundColor = c;
+            }
+
+        }
+
+        private static double DoubleParse(string x) => double.Parse(x, CultureInfo.InvariantCulture);
+
+        private static T[] ParseCC<T>(string[] rawInput, int requiredLength, Func<string[], T> factory)
+        {
+            var result = new List<T>();
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                var parts = rawInput[i].Split(' ');
+
+
+                if (parts.Length != requiredLength)
+                {
+                    continue;
+                }
+                result.Add(factory(parts));
+            }
+
+            return result.ToArray();
         }
 
         private static double Grd2Rad(double v) => v * (2d * PI) / 360d;
@@ -31,9 +91,21 @@ namespace HelloBla
             double alpha = distance / turnRadios;
 
             double s = 2d * turnRadios * Sin(alpha / 2d);
+            if (turnRadios == double.PositiveInfinity)
+            {
+                s = 1;
+            }
             double beta = (PI - alpha) / 2d;
 
-            return (Round(s * Cos(beta), 2), Round(s * Sin(beta), 2), Rad2Grd(beta - steeringAngle));
+            var newDirection = alpha;
+
+            while (newDirection < 0) newDirection += FULLCIRCLE;
+            while (newDirection >= FULLCIRCLE) newDirection -= FULLCIRCLE;
+
+
+            var y_2 = 2 * turnRadios * Sin(alpha / 2);
+
+            return (s * Cos(beta), s * Sin(beta), Rad2Grd(newDirection));
         }
 
         static (double x, double y, double newSteeringAngle) Level2(double wheelBase, double distance, double steeringAngle)
