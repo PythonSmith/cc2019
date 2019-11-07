c052f1008be7a7c2910cde8705d703212c27c520
Benedikt Schlager <benedikt.schlager@outlook.at>
2019-11-07 20:57:49 +0100

    Refactoring and fix last of level 2

---
+++ HelloBla/CcHelpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace HelloBla
+{
+    class CcHelpers
+    {
+        public static string AsCCDouble(double x) => string.Format(CultureInfo.InvariantCulture, "{0:0.00}", x);
+
+        public static void WriteLineCC((double WheelBase, double Distance, double SteeringAngle) i, bool output = false)
+        {
+            var c = Console.ForegroundColor;
+            if (output)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.WriteLine($"{AsCCDouble(i.WheelBase)} {AsCCDouble(i.Distance)} {AsCCDouble(i.SteeringAngle)}");
+            if (output)
+            {
+                Console.ForegroundColor = c;
+            }
+
+        }
+
+        public static double DoubleParse(string x) => double.Parse(x, CultureInfo.InvariantCulture);
+
+        public static T[] ParseCC<T>(string[] rawInput, int requiredLength, Func<string[], T> factory)
+        {
+            var result = new List<T>();
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                var parts = rawInput[i].Split(' ');
+
+
+                if (parts.Length != requiredLength)
+                {
+                    continue;
+                }
+                result.Add(factory(parts));
+            }
+
+            return result.ToArray();
+        }
+    }
+}
--- HelloBla/Program.cs
+++ HelloBla/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using static System.Math;
+using static HelloBla.CcHelpers;
 
 namespace HelloBla
 {
@@ -11,6 +12,14 @@ namespace HelloBla
 
         static void Main(string[] args)
         {
+            DoLevel2();
+
+
+            //Console.WriteLine(Level2Neu(9.53d, 8.12d, 0.00d));
+        }
+
+        private static void DoLevel2()
+        {
             var rawInput = @"1.00 1.00 30.00
 2.13 4.30 23.00
 1.75 3.14 -23.00
@@ -19,64 +28,12 @@ namespace HelloBla
 9.53 8.12 0.00"; // -> 0.00 8.12 0.00
 
             var input = ParseCC(rawInput.Split('\n'), 3, a => (WheelBase: DoubleParse(a[0]), Distance: DoubleParse(a[1]), SteeringAngle: DoubleParse(a[2])));
-
-            /*
-            Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(1, 13.76d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(1, 2.34d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(1, 90d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(2.45, 90d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
-            Console.WriteLine(Turnradius(1, 30d / 360 * 2 * PI));
-            */
             Console.WriteLine("Level 2");
             foreach (var i in input)
             {
                 WriteLineCC(i);
                 WriteLineCC(Level2Neu(i.WheelBase, i.Distance, Grd2Rad(i.SteeringAngle)), true);
             }
-           
-
-            //Console.WriteLine(Level2Neu(9.53d, 8.12d, 0.00d));
-        }
-
-        private static string AsCCDouble(double x) => string.Format(CultureInfo.InvariantCulture, "{0:0.00}", x);
-
-        private static void WriteLineCC((double WheelBase, double Distance, double SteeringAngle) i, bool output=false)
-        {
-            var c = Console.ForegroundColor;
-            if (output)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-            }
-
-            Console.WriteLine($"{AsCCDouble(i.WheelBase)} {AsCCDouble(i.Distance)} {AsCCDouble(i.SteeringAngle)}");
-            if (output)
-            {
-                Console.ForegroundColor = c;
-            }
-
-        }
-
-        private static double DoubleParse(string x) => double.Parse(x, CultureInfo.InvariantCulture);
-
-        private static T[] ParseCC<T>(string[] rawInput, int requiredLength, Func<string[], T> factory)
-        {
-            var result = new List<T>();
-            for (int i = 0; i < rawInput.Length; i++)
-            {
-                var parts = rawInput[i].Split(' ');
-
-
-                if (parts.Length != requiredLength)
-                {
-                    continue;
-                }
-                result.Add(factory(parts));
-            }
-
-            return result.ToArray();
         }
 
         private static double Grd2Rad(double v) => v * (2d * PI) / 360d;
@@ -93,7 +50,7 @@ namespace HelloBla
             double s = 2d * turnRadios * Sin(alpha / 2d);
             if (turnRadios == double.PositiveInfinity)
             {
-                s = 1;
+                s = distance;
             }
             double beta = (PI - alpha) / 2d;
 
